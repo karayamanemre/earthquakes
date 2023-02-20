9af329511452ee319333289940da9142508b798a
Emre Karayaman <107217989+karayamanemre@users.noreply.github.com>
2023-02-20 16:34:46 +0300

    style updates

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const apiUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 const earthquakeListElement = document.getElementById('earthquake-list');
-const paginationElement = document.querySelector('.pagination');
+const paginationElement1 = document.getElementById('pagination-1');
+const paginationElement2 = document.getElementById('pagination-2');
 const loadingSpinner = document.getElementById('loading-spinner');
 loadingSpinner.classList.remove('hidden');
 
@@ -15,19 +16,34 @@ fetch(apiUrl)
     const pageNumbers = Array.from({length: numPages}, (_, i) => i + 1);
     showPage(earthquakes, currentPageNumber);
 
-    paginationElement.innerHTML = pageNumbers.map(pageNumber => `
+    paginationElement1.innerHTML = pageNumbers.map(pageNumber => `
       <li class="page-item${pageNumber === currentPageNumber ? ' active' : ''}">
         <a class="page-link p-2 rounded bg-gray-300 text-gray-700 hover:bg-gray-400 transition duration-500" href="#${pageNumber}">${pageNumber}</a>
       </li>
     `).join('');
 
-    paginationElement.addEventListener('click', event => {
+    paginationElement1.addEventListener('click', event => {
       event.preventDefault();
       const pageNumber = parseInt(event.target.getAttribute('href').substring(1));
       showPage(earthquakes, pageNumber);
       currentPageNumber = pageNumber;
       updatePagination();
     });
+
+    paginationElement2.innerHTML = pageNumbers.map(pageNumber => `
+      <li class="page-item${pageNumber === currentPageNumber ? ' active' : ''}">
+        <a class="page-link p-2 rounded bg-gray-300 text-gray-700 hover:bg-gray-400 transition duration-500" href="#${pageNumber}">${pageNumber}</a>
+      </li>
+    `).join('');
+
+    paginationElement2.addEventListener('click', event => {
+      event.preventDefault();
+      const pageNumber = parseInt(event.target.getAttribute('href').substring(1));
+      showPage(earthquakes, pageNumber);
+      currentPageNumber = pageNumber;
+      updatePagination();
+    });
+
 
     earthquakeListElement.addEventListener('click', event => {
       const target = event.target.closest('.earthquake');
@@ -61,7 +77,7 @@ function showPage(earthquakes, pageNumber) {
   const startIndex = (pageNumber - 1) * earthquakesPerPage;
   const endIndex = startIndex + earthquakesPerPage;
   const displayedEarthquakes = earthquakes.slice(startIndex, endIndex);
-
+  window.scrollTo(0, 0);
   earthquakeListElement.innerHTML = displayedEarthquakes.map(e => {
     const properties = e.properties;
     const magnitude = properties.mag;
